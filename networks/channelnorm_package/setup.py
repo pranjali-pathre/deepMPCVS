3e16e0fd2bf56d6bf865d7efbeafb3f6f5d19465
Vedansh Mittal <56213441+VedanshM@users.noreply.github.com>
2023-01-25 17:54:24 +0530

    tweaked for installation

--- networks/channelnorm_package/setup.py
+++ networks/channelnorm_package/setup.py
@@ -5,9 +5,10 @@ import torch
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
-cxx_args = ['-std=c++11']
+cxx_args = ['-std=c++14']
 
 nvcc_args = [
+    '-gencode', 'arch=compute_50,code=sm_50',
     '-gencode', 'arch=compute_52,code=sm_52',
     '-gencode', 'arch=compute_60,code=sm_60',
     '-gencode', 'arch=compute_61,code=sm_61',
